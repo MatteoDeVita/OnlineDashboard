5a737ada8c3794650b1b709a9737b64f95dfbed1
k4lii <71388385+k4lii@users.noreply.github.com>
2020-11-16 16:48:10 +0100

    Update main.py

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,35 +1,10 @@
-from flask import Blueprint, render_template 
-from flask_login import login_required, current_user
+from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
-main = Blueprint('main', __name__)
+def hello():
+    return "Hello World from Flask"
 
-@main.route('/')
-def index():
-    return render_template('index.html')
-
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
-
-@main.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
-
-if name == "main":
+if __name__ == "__main__":
     # Only for debugging while developing
-   app.run(host='0.0.0.0', debug=True, port=5000)
-
-
-#from flask import Flask
-#app = Flask(__name__)
-#
-#@app.route("/")
-#def hello():
-#    return "Hello World from Flask"
-#
-#if __name__ == "__main__":
-#    # Only for debugging while developing
-#    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
