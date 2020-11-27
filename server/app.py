07db1b415de860e158bcd88dedc6dad73e60ebe3
k4lii <71388385+k4lii@users.noreply.github.com>
2020-11-27 12:26:52 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,38 +1,51 @@
-from flask import Flask, jsonify, redirect, url_for
-# from flask_mysqldb import MySQL
+from flask import Flask, request, flash, url_for, redirect, render_template, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
-import requests
-#configuration
-DEBUG = True
+from flask_sqlalchemy import SQLAlchemy
+import json
+import sys
 
 #instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
-# mysql = MySQL(app)
+app.secret_key = "hello"
+#app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/users.db'
 
-app.config['MYSQL_HOST'] = 'db'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'hourquet'
-app.config['MYSQL_DB'] = 'dash_db'
-
-# mysql = MySQL(app)
-
+db = SQLAlchemy(app)
 #enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+class Users(db.Model):
+   id = db.Column('id', db.Integer, primary_key = True)
+   user = db.Column(db.String(100))
+   passw = db.Column(db.String(100))
+
+def __init__(self, user, passw):
+   self.user = user
+   self.passw = passw
 
 @app.route('/')
 def root():
     return redirect(url_for('login'))
 
-
-@app.route('/login')
-def login():
-    return 'login'
+@app.route('/login/<username>/<password>')
+def login(username, password):
+    #value = {'username': username, 'password': password}
+    new_user = Users(user=username, passw=password)
+    db.session.add(new_user)
+    db.session.commit()
+    #Users.query.order_by(Users.username).all()
+    print(Users.query.order_by(Users.user).all(), flush=True)
+    print(new_user.user, flush=True)
+    #jsonvalue = jsonify(value)
+    #console.log()
+    return "coucou"
+    #return login
 
 # sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return 'ppong!! salut a tous'
+#@app.route('/ping', methods=['GET'])
+#def ping_pong():
+#    return 'ppong!! salut a tous'
 
 @app.route('/youtubeSearch/<querry>')
 def youtubeSearch(querry):
@@ -106,4 +119,5 @@ def weatherForecast(city):
     return jsonify(dic)
 
 if __name__ == '__main__':
-        app.run(host="0.0.0.0", debug=True)
+  db.create_all()
+  app.run(host="0.0.0.0", debug=True)
